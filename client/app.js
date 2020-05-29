400d9788a7dcf1941c59b7f623239f2e4b9452d3
Josh Melo <joshuamelo1126@gmail.com>
2020-05-29 00:41:08 -0400

    added the footer component

--- client/app.js
+++ client/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
     <div>
       <Header />
       Middle of the page stuff
+      <Footer />
     </div>
+
   );
 };
 
