80ce088360050b42dc638550b422a6d0c7e25481
Amy Liao <a.liao.uey@gmail.com>
2020-06-01 00:00:42 -0400

    Added dummy data and added db seed functionality.

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@ const Address = require('./models/address');
 const Orders = require('./models/orders');
 const Products = require('./models/products');
 const Users = require('./models/users');
+const productDummyData = require('./productDummyData');
 
 Users.hasMany(Address);
 Address.belongsTo(Users);
@@ -10,5 +11,36 @@ Users.hasMany(Orders);
 Orders.belongsTo(Users);
 Orders.hasMany(Products);
 
-db.sync({ force: true });
-console.log('Synced DB');
+async function seed() {
+  await db.sync({ force: true });
+  console.log('db synced!');
+
+  const products = await Promise.all(
+    productDummyData.map(product => {
+      return Products.create(product);
+    })
+  );
+
+  console.log(`${products.length} products`);
+  console.log(`seeded successfully`);
+}
+
+async function runSeed() {
+  console.log('...seeding');
+  try {
+    await seed();
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  } finally {
+    console.log('closing db connection');
+    await db.close();
+    console.log('db connection closed');
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+if (module === require.main) runSeed();
+
+module.exports = seed;
