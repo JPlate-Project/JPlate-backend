660ce753899d81739f54914536e0bdfb719c0b2a
Josh Melo <joshuamelo1126@gmail.com>
2020-06-03 23:00:28 -0400

    mapped plates to plate component

--- client/app.js
+++ client/app.js
@@ -2,22 +2,46 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Plate from './components/Plate.jsx';
-
+import axios from 'axios'
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      plates: null
+    }
 
+  }
+  async componentDidMount() {
+
+    const response = await axios.get('/getPlates')
+    this.setState({
+      plates: response.data
+    })
+
+  }
   render() {
-    return (
-      <div>
-        <Header />
-        <div className="plateContainer">
-          <Plate />
-          <Plate />
-          <Plate />
-          <Plate />
+
+    if (this.state.plates) {
+      return (
+        <div>
+
+          <Header />
+          <div className="plateContainer">
+            {this.state.plates.map(plate => { return <Plate currentPlate={plate} /> })}
+
+
+
+
+
+
+
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    )
+      )
+    } else {
+      return (<div>Loading...</div>)
+    }
   }
 }
 
--- client/components/Plate.jsx
+++ client/components/Plate.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 
-const Plate = () => {
-  return (
-    <div className="singlePlate">
-      <h3>Blue Plate</h3>
-      A beautiful blue plate
-      <hr></hr>
-      <img
-        src='https://photoshopdoctor.files.wordpress.com/2016/07/3-blue-plate.jpg?w=723'
-        alt='cannot display'
-        height='200'
-        width='200'
-      />
-      <br></br>
-      <a href="page">Click for details</a>
-      <br></br>
-      <button>Add to cart</button>
-    </div>
-  );
+const Plate = (props) => {
+  console.log(props.currentPlate)
+  if (props.currentPlate.id) {
+    return (
+      <div className="singlePlate">
+        <img
+          src={props.currentPlate.imageURL}
+          alt='cannot display'
+          height='200px'
+          width='200px'
+        />
+        <br></br>
+        <h4>  {props.currentPlate.name}</h4>
+        {props.currentPlate.description}
+
+
+        <br></br>
+
+        <button><img src='https://previews.123rf.com/images/aguiters/aguiters1711/aguiters171100033/90038691-shopping-cart-icon-vector.jpg' height='20px' width='20px' /></button>
+        {`$${props.currentPlate.price}`}
+      </div>
+    );
+  } else {
+    return (<div>loading</div>)
+  }
 };
 
 export default Plate;
