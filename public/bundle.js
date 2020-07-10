4f717464767e7101f80a29478f5a3089b82942d4
Josh Melo <joshuamelo1126@gmail.com>
2020-07-09 22:54:05 -0400

    added stuff

--- public/bundle.js
+++ public/bundle.js
@@ -132,45 +132,7 @@ var Cart = function Cart(props) {
       key: Math.random()
     }, item.name, item.userSelectedQuantity);
   }) : "There are no items in the cart", sum);
-}; // const Cart = (props) => {
-//   if (!(props.cart.length)) {
-//     return (
-//       <div id="emptyCart">
-//         <button type="button" onClick={props.toggleCart}><img src="http://icons.iconarchive.com/icons/icons8/windows-8/256/Arrows-Left-Arrow-icon.png" height="20px" width="20px" /></button>
-//         Cart is empty
-//       </div>
-//     );
-//   }
-//   return (
-//     <div id="cart">
-//       <button onClick={props.toggleCart}><img src="http://icons.iconarchive.com/icons/icons8/windows-8/256/Arrows-Left-Arrow-icon.png" height="20px" width="20px" /></button>
-//       <div id="cartItems">
-//         <table>
-//           <tr>
-//             <th>Name</th>
-//             <th />
-//             <th>Price</th>
-//             <th>Quantity</th>
-//             <th />
-//           </tr>
-//           {props.cart.map(item => {
-//             return (
-//               <React.Fragment>
-//                 <tr>
-//                   <td>{item.name}</td>
-//                   <td><img src={item.imageURL} height="40px" width="40px" /></td>
-//                   <td>${item.price}</td>
-//                   <td>0<button>+</button><button>-</button></td>
-//                   <td><button onClick={() => { return props.removeItem(item.id); }}><img src="https://cdn4.iconfinder.com/data/icons/controls-add-on-flat/48/Contols_-_Add_On-35-512.png" height="40px" width="40px" /></button></td>
-//                 </tr>
-//               </React.Fragment>);
-//           })}
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
-
+};
 
 /* harmony default export */ __webpack_exports__["default"] = (Cart);
 
