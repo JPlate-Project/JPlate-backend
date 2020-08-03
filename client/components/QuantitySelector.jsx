2d706c0fd98f33fccd3d71982eb5e90f5179d5c5
Josh Melo <joshuamelo1126@gmail.com>
2020-08-02 20:39:35 -0400

    styling and fixing of cart

--- client/components/QuantitySelector.jsx
+++ client/components/QuantitySelector.jsx
@@ -4,10 +4,13 @@ const QuantitySelector = (props) => {
 
   const selectorRef = useRef(null);
   const currentQuantity = props.item.userSelectedQuantity;
-  const validQuantityRange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const validQuantityRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   const handleSelect = () => {
     const selectedQuantity = selectorRef.current.selectedIndex;
+    if (selectedQuantity === 0) {
+      props.handleCartRemove(props.item);
+    }
     props.handleItemQuantityChangeCart(props.item, selectedQuantity);
   };
 
