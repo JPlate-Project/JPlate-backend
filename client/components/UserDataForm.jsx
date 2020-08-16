560c4c88a8ed8da0fabc13603c5a8296e7f48f7f
Josh Melo <joshuamelo1126@gmail.com>
2020-08-16 01:24:25 -0400

    added userdataform

--- client/components/UserDataForm.jsx
+++ client/components/UserDataForm.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const UserDataForm = () => {
+
+  const formRef = useRef(null);
+
+  function handleSubmit() {
+    event.preventDefault();
+    const orderObj = {
+      
+    }
+    console.log(formRef.current[0].value)
+    console.log('hello')
+  }
   return (
     <div>
-      <div className="signInContainer">
-        <label>Email</label>
-        <input type="text" />
-        <label>First Name</label>
-        <input type="text" />
-      </div>
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <div className="checkoutNoSignIn">
+          <label>First and last Name</label>
+          <input type="text" />
+          <label>Email Address</label>
+          <input type="text" />
+          <label>Shipping Address</label>
+          <input type="text" />
+          <label>Credit Card</label>
+          <input type="text" />
+          <button type="submit" className="submitOrderButton">Submit order</button>
+        </div>
+      </form>
     </div>
   );
 };
