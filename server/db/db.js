9faf39e0834ff794e7151262aa4eebddb3555bd3
Josh Melo <joshuamelo1126@gmail.com>
2020-09-22 23:36:58 -0400

    test

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,9 @@ const dbUrl = process.env.DATABASE_URL;
 const db = new Sequelize(
   'postgres://lqzklnfccmzwxx:bbca2824645f4a8856884d8dc29c1c3a921b3c1dd82144b5c7531c4588e7320d@ec2-34-230-231-71.compute-1.amazonaws.com:5432/df1a0ott3es0vr',
   {
-    logging: false,
+    logging: true,
   }
 );
 
+console.log('hello');
 module.exports = db;
