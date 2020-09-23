d6dabc0538fad1470cd7aeaef389329212d60b62
Josh Melo <joshuamelo1126@gmail.com>
2020-09-22 23:57:01 -0400

    stff

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const dbUrl = process.env.DATABASE_URL;
 
 const db = new Sequelize(
   'postgres://lqzklnfccmzwxx:bbca2824645f4a8856884d8dc29c1c3a921b3c1dd82144b5c7531c4588e7320d@ec2-34-230-231-71.compute-1.amazonaws.com:5432/df1a0ott3es0vr',
@@ -8,5 +7,4 @@ const db = new Sequelize(
   }
 );
 
-console.log(db);
 module.exports = db;
--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,16 @@
-const db = require('../db/db');
+
 const Address = require('./models/address');
 const Orders = require('./models/orders');
 const Products = require('./models/products');
 const Users = require('./models/users');
 const productDummyData = require('./productDummyData');
+
+const db = new Sequelize(
+  'postgres://lqzklnfccmzwxx:bbca2824645f4a8856884d8dc29c1c3a921b3c1dd82144b5c7531c4588e7320d@ec2-34-230-231-71.compute-1.amazonaws.com:5432/df1a0ott3es0vr',
+  {
+    logging: false,
+  }
+);
 
 Users.hasMany(Address);
 Address.belongsTo(Users);
