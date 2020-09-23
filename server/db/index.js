4c4ef0da33c62425f330494dd44b635d5dd364e2
Josh Melo <joshuamelo1126@gmail.com>
2020-09-23 00:01:39 -0400

    stff

--- server/db/index.js
+++ server/db/index.js
@@ -1,25 +1,18 @@
+const db = require('./db');
 const Address = require('./models/address');
 const Orders = require('./models/orders');
 const Products = require('./models/products');
 const Users = require('./models/users');
 const productDummyData = require('./productDummyData');
-const Sequelize = require('sequelize');
-
-const db = new Sequelize(
-  'postgres://lqzklnfccmzwxx:bbca2824645f4a8856884d8dc29c1c3a921b3c1dd82144b5c7531c4588e7320d@ec2-34-230-231-71.compute-1.amazonaws.com:5432/df1a0ott3es0vr',
-  {
-    logging: false,
-  }
-);
 
 Users.hasMany(Address);
 Address.belongsTo(Users);
 Users.hasMany(Orders);
 Orders.belongsTo(Users);
 Orders.hasMany(Products);
-db.sync({ force: true });
 
 async function seed() {
+  db.sync();
   console.log('db synced!');
 
   const products = await Promise.all(
