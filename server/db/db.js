91cc2e309bda57abe23f6aea70682eec299ebe6a
Josh Melo <joshuamelo1126@gmail.com>
2020-09-22 22:43:16 -0400

    db fixes

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/jplate`;
-console.log(process.eventNames.DATABASE_URL);
+console.log('database url', process.env.DATABASE_URL);
+console.log('process env', process.env);
+console.log('proces', process);
 const db = new Sequelize(dbUrl, {
   logging: false,
 });
