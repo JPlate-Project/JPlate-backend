10d1012c0d4c0ba119ff4d4df7d4890133f1a734
Josh Melo <joshuamelo1126@gmail.com>
2020-09-22 23:58:10 -0400

    stff

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
-
 const Address = require('./models/address');
 const Orders = require('./models/orders');
 const Products = require('./models/products');
 const Users = require('./models/users');
 const productDummyData = require('./productDummyData');
+const Sequelize = require('sequelize');
 
 const db = new Sequelize(
   'postgres://lqzklnfccmzwxx:bbca2824645f4a8856884d8dc29c1c3a921b3c1dd82144b5c7531c4588e7320d@ec2-34-230-231-71.compute-1.amazonaws.com:5432/df1a0ott3es0vr',
