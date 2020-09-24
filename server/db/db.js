a6ac65fb74af16c8ca76b377c92c5b8fc75a9a4f
Josh Melo <joshuamelo1126@gmail.com>
2020-09-23 22:29:05 -0400

    Fun stuff

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 const dbUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/jplate';
-const db = new Sequelize(dbUrl, { logging: false });
+const db = new Sequelize(dbUrl, { logging: console.log });
 
 module.exports = db;
