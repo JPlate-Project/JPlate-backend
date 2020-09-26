94c6bcd841801325832d5cad4a74d7ce1611e900
Amy Liao <a.liao.uey@gmail.com>
2020-09-26 00:47:55 -0400

    eat

--- server/db/db.js
+++ server/db/db.js
@@ -2,4 +2,13 @@ const Sequelize = require('sequelize');
 const dbUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/jplate';
 const db = new Sequelize(dbUrl, { logging: console.log });
 
+async function testDb() {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+testDb();
 module.exports = db;
