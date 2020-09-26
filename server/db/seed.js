a53039f379fa1f831235ffc97bcc9c245583d2c7
Amy Liao <a.liao.uey@gmail.com>
2020-09-26 01:22:16 -0400

    stuff

--- server/db/seed.js
+++ server/db/seed.js
@@ -28,10 +28,6 @@ async function runSeed() {
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
-  } finally {
-    console.log('closing db connection');
-    await db.close();
-    console.log('db connection closed');
   }
 }
 
