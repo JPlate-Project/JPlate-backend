0c0a1dabe42a6c261e90427ec1888799d451bab7
Amy Liao <a.liao.uey@gmail.com>
2020-09-26 00:54:26 -0400

    eat

--- server/db/db.js
+++ server/db/db.js
@@ -5,7 +5,8 @@ const db = new Sequelize(dbUrl, { logging: console.log });
 async function testDb() {
   console.log('right before connecting')
   try {
-    await db.authenticate();
+    console.log(db.authenticate());
+    db.authenticate()
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
