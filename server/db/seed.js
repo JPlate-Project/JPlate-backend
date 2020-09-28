580b64215d71d624638fc88f999f67127c132f96
Amy Liao <a.liao.uey@gmail.com>
2020-09-28 00:33:24 -0400

    db

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,7 +2,7 @@ const db = require('./db');
 const { Product } = require('./models/index');
 const productDummyData = require('./productDummyData');
 
-function seed() {
+async function seed() {
   // const products = await Promise.all(
   //   productDummyData.map(product => {
   //     return Product.create(product);
@@ -10,21 +10,24 @@ function seed() {
   // );
   // console.log(`seeded ${products.length} plates successfully!`);
 
-  // Product.create({
+  const products = await Promise.all(
+    [Product.create({
+      name: 'Plain White Plate',
+      price: 25.00,
+      quantity: 100,
+      imageURL: 'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/202009/3479/img4o.jpg',
+      description: 'Classic round white dining plate.',
+    })]
+  );
+  console.log(`seeded ${products.length} plates successfully!`);
+
+  // console.log('1', Product, '2', typeof Product, '3', Product.create, '4', Product.create({
   //   name: 'Plain White Plate',
   //   price: 25.00,
   //   quantity: 100,
   //   imageURL: 'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/202009/3479/img4o.jpg',
   //   description: 'Classic round white dining plate.',
-  // });
-
-  console.log('1', Product, '2', typeof Product, '3', Product.create, '4', Product.create({
-    name: 'Plain White Plate',
-    price: 25.00,
-    quantity: 100,
-    imageURL: 'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/202009/3479/img4o.jpg',
-    description: 'Classic round white dining plate.',
-  }));
+  // }));
   console.log('wtf mang!');
 }
 
