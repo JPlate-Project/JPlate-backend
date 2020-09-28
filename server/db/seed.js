ea8611b33d23586b4ab4994dbcba2d4f9edb7f34
Amy Liao <a.liao.uey@gmail.com>
2020-09-28 00:18:38 -0400

    db

--- server/db/seed.js
+++ server/db/seed.js
@@ -17,7 +17,14 @@ function seed() {
   //   imageURL: 'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/202009/3479/img4o.jpg',
   //   description: 'Classic round white dining plate.',
   // });
-  console.log('1', Product, '2', typeof Product, '3', Product.create, '4', Product.create({}));
+
+  console.log('1', Product, '2', typeof Product, '3', Product.create, '4', Product.create({
+    name: 'Plain White Plate',
+    price: 25.00,
+    quantity: 100,
+    imageURL: 'https://www.potterybarn.com/pbimgs/ab/images/dp/wcm/202009/3479/img4o.jpg',
+    description: 'Classic round white dining plate.',
+  }));
   console.log('wtf mang!');
 }
 
