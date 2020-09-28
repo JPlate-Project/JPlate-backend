0c7eb73545401da1a3ee8b838e8e71406d3407b6
Amy Liao <a.liao.uey@gmail.com>
2020-09-27 23:26:50 -0400

    db test

--- server/db/seed.js
+++ server/db/seed.js
@@ -3,21 +3,18 @@ const Product = require('./models/products');
 const productDummyData = require('./productDummyData');
 
 async function seed() {
-  db.authenticate();
-
   const products = await Promise.all(
     productDummyData.map(product => {
       return Product.create(product);
     })
   );
-  console.log("did i make it here")
   console.log(`seeded ${products.length} plates successfully!`);
 }
 
-async function runSeed() {
+function runSeed() {
   console.log('...seeding');
   try {
-    await seed();
+    seed();
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
