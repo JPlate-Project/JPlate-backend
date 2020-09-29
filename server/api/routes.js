b78338fbb56fffe8a35409494fc9f82f0db2d43b
Joshua Melo <joshuamelo@gmail.com>
2020-09-29 15:50:20 -0400

    testing fix

--- server/api/routes.js
+++ server/api/routes.js
@@ -35,7 +35,7 @@ router.patch('/resetPassword', async (req, res, next) => {
 
 router.get('/getPlates', async (req, res, next) => {
   try {
-    const plates =  Product.findAll();
+    const plates =  await Product.findAll();
     res.send(plates);
   } catch (err) {
     next(err);
