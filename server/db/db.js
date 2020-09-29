bcc9752ae6c9097147a3ec7ff3bc228b04f75fd9
Joshua Melo <joshuamelo@gmail.com>
2020-09-29 17:05:38 -0400

    testing fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,33 +1,7 @@
 const Sequelize = require('sequelize');
 const dbURL = process.env.DATABASE_URL || 'postgres://localhost:5432/jplate';
 
-async function establishDB() {
-  const db = await new Sequelize(dbURL, {
-    // host: 'ec2-34-198-243-120.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: true,
-    },
-  });
+const db =  new Sequelize(dbURL);
 
-  db.authenticate().then(() => {
-    console.log('authenticated')
-  }).catch(err => console.log(err))
-
-  console.log(db)
-
-}
-
-establishDB()
-// function authenticateDB(db) {
-//   try {
-//     db.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// }
-// authenticateDB();
-
-// module.exports = db;
+console.log(db)
+module.exports = db;
