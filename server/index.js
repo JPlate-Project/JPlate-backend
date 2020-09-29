da89f9b60227a57b9e4f6ac8fb39cabf076922c1
Amy Liao <a.liao.uey@gmail.com>
2020-09-29 18:27:55 -0400

    testing fix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./api/routes');
 const { db } = require('./db/db2');
-module.exports = app;
 
 const createApp = () => {
   //Allow cors
@@ -55,12 +54,11 @@ const createApp = () => {
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
   const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
-  );
+    console.log(`Mixing it up on port ${PORT}`));
 };
 
 async function bootApp() {
-  await db.sync();
+  await db.sync({force: true});
   await createApp();
   await startListening();
 }
