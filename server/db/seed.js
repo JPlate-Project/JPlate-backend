4119ab71526c20b6983cd7dcfd1bad9f0ac625c4
Amy Liao <a.liao.uey@gmail.com>
2020-09-29 17:51:31 -0400

    testing fix

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { db, Product } = require('../db/db2');
 const productDummyData = require('./productDummyData');
 
-console.log('db from seed file', db);
+console.log('What is? --->', db.sync);
 
 const seed = async () => {
   await db.sync({ force: true });
@@ -16,7 +16,7 @@ const seed = async () => {
   );
 
   console.log(`seeded ${products.length} plates successfully!`);
-  db.close()
+  db.close();
 };
 
 seed().catch(err => {
