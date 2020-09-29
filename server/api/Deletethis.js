0387dd08ead9a6b7a5fae8179ed2a1fcabe58d67
Joshua Melo <joshuamelo@gmail.com>
2020-09-29 16:10:49 -0400

    testing fix

--- server/api/Deletethis.js
+++ server/api/Deletethis.js
@@ -1,15 +1,10 @@
-
 const Product = require('../db/models/products');
 
 async function test(){
-    console.log('before functioin')
-    const plates =  Product.findAll().then( () => {
-        console.log("we made it ")
-    }).catch(err => console.log(err));
-
+    console.log('before await')
+    const plates =  await Product.findAll();
+    console.log('after await')
     console.log(plates)
-
-
 }
 
 test()
