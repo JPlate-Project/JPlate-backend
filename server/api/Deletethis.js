f97b222edef67e91e18eb8e5bc4917e2e6355600
Joshua Melo <joshuamelo@gmail.com>
2020-09-29 16:26:01 -0400

    testing fix

--- server/api/Deletethis.js
+++ server/api/Deletethis.js
@@ -1,10 +1,10 @@
-const Product = require('../db/models/products');
+// const Product = require('../db/models/products');
 const fetch = require("node-fetch");
 async function test(){
     console.log('before await')
     const plates =  await fetch('https://dog.ceo/api/breeds/image/random');
     console.log('after await')
-    console.log(plates)
+    console.log(await plates.json())
 }
 
 test()
