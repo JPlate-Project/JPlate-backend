5ee2e5dd18604817d019c36e9d7cb5487544838a
Amy Liao <a.liao.uey@gmail.com>
2020-10-22 23:05:34 -0400

    about me

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -6,6 +6,9 @@ const About = () => {
     <>
       <Header />
       <h1> About Page</h1>
+      <p>
+        This is version 1 of JPlate. We are using React JS, Express, and Postgres for db.
+      </p>
     </>
   );
 };
